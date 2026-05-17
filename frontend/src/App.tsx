659347ee5bd7f286fafd21c1684c3857522c31be
sudo-Harshk <harshk1744@gmail.com>
2026-05-17 23:13:31 +0530

    modified:   frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,7 +19,7 @@ type Question = {
 };
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || '',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 // ProgressBar component
